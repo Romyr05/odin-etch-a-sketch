1cf7515fd28c81dce4a7ef9f79add166bc219d81
Romyr <romyrlopez123@gmail.com>
2025-08-19 23:53:05 +0800

    made a 16by16 box

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const container = document.getElementById("container")
 
 const size = 16;
-const cell_size = 20;
 
-for(let i = 0; i<16*16; i++){
-    const cell = document.createElement("div")
-    cell.className = ("cells")
-    cell.style.border =  "solid"
-    cell.style.width = "20px"
-    cell.style.height = "20px"
-    cell.style.borderWidth = "1px"
-    container.appendChild(cell)
+function add_grid(size){
+    const container_size = container.offsetWidth + container.offsetHeight
+    const cell_size = (container_size/ size) - 2
+
+    for(let i = 0; i<size*size; i++){
+        const cell = document.createElement("div")
+        cell.className = ("cells")
+        cell.style.border =  "solid"
+        cell.style.width = `${cell_size}px`
+        cell.style.height = `${cell_size}px`
+        cell.style.borderWidth = "1px"
+        container.appendChild(cell)
+    }
 }
+
+add_grid(size)
+
+
 
 
 
